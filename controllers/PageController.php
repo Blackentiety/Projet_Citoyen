1da09ed2201d61a5f28882ba3ff80b9ba1330d05
Lukas <lukas.boudinl@gmail.com>
2025-04-08 14:22:51 +0200

    ajout des méthodes about, blog et form dans PageController

--- controllers/PageController.php
+++ controllers/PageController.php
@@ -6,4 +6,20 @@ class PageController{
         require_once('views/home.php');
         require_once('templates/GlobalTemplate.php');
     }
+
+    public static function about(){
+        require_once('views/about.php');
+        require_once('templates/GlobalTemplate.php');
+    }
+
+    public static function blog(){
+        require_once('views/blog.php');
+        require_once('templates/GlobalTemplate.php');
+    }
+    
+    public static function form(){
+        require_once('views/form.php');
+        require_once('templates/GlobalTemplate.php');
+    }
+
 }
