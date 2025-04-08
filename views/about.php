cb64a11f80ed4ab3f157a89a8480679015c09ddd
Yanis <loze.yanis@gmail.com>
2025-04-08 16:20:25 +0200

    Ajout de la vue "À propos" avec contenu sur la mission et l'équipe

---
+++ views/about.php
@@ -0,0 +1,12 @@
+<?php
+$HeadTitle = "À propos de nous";
+$mainTitle = "À propos de nous";
+ob_start();
+?>
+
+<div>
+    <h2>Notre mission</h2>
+    <p>Nous sommes dédiés à fournir des informations précises et utiles sur l'entrepreneuriat féminin.</p>
+    <h2>Notre équipe</h2>
+    <p>Notre équipe est composée d'experts en entrepreneuriat, de chercheurs et de passionnés qui travaillent ensemble pour soutenir les femmes entrepreneures.</p>
+</div>
