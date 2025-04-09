9351b4b09a897f6801462bf62c4640721df96777
yelnnee <y.adouane@ecole-ipssi.net>
2025-04-09 14:07:10 +0200

    g

--- views/form.php
+++ views/form.php
@@ -4,8 +4,6 @@ $mainTitle = 'Formulaire';
 ob_start();
 ?>
 <section class="main-section">
-    <h2 class="main-section-title">Formulaire</h2>
-    
     <div class="formulaire-container">
         <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdwi1byaZxobYv6jaPSm_AMsgmq4VBreYXxWko-MPKg7I1Xbg/viewform?embedded=true" 
             width="640" height="1855" frameborder="0" marginheight="0" marginwidth="0">
