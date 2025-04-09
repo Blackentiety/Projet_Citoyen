f4a021280cb419ad1b1c0ccef87b96b8464f3d84
yelnnee <y.adouane@ecole-ipssi.net>
2025-04-09 17:28:42 +0200

    ajout partie nos valeurs

--- views/about.php
+++ views/about.php
@@ -5,10 +5,33 @@ ob_start();
 ?>
 
 <div class="a-propos">
+    <div class ="missions">
     <h2>Notre mission</h2>
     <p>Nous sommes dédiés à fournir des informations précises et utiles sur l'entrepreneuriat féminin.</p>
     <h2>Notre équipe</h2>
     <p>Notre équipe est composée d'experts en entrepreneuriat, de chercheurs et de passionnés qui travaillent ensemble pour soutenir les femmes entrepreneures.</p>
+</div>
+    <section class="valeurs">
+    <h2>Nos valeurs</h2>
+    <div class="valeurs-container">
+        <div class="valeur">
+            <h3>💬 Sororité</h3>
+            <p>Nous croyons en la force du collectif féminin. Se soutenir, s'entraider, partager pour grandir ensemble.</p>
+        </div>
+        <div class="valeur">
+            <h3>🔎 Transparence</h3>
+            <p>Nous partageons des ressources fiables, honnêtes et authentiques, sans filtres ni faux-semblants.</p>
+        </div>
+        <div class="valeur">
+            <h3>🌱 Évolution</h3>
+            <p>Ce blog est un espace d’apprentissage où chacune peut progresser à son rythme, sans pression.</p>
+        </div>
+        <div class="valeur">
+            <h3>🧡 Bienveillance</h3>
+            <p>Notre communauté repose sur le respect, l’écoute et l’inclusivité. Chaque voix compte.</p>
+        </div>
+    </div>
+</section>    
 </div>
 
 <?php
