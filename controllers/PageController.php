301229a50048ceab613eec7062b2a9148f162336
Lukas <lukas.boudinl@gmail.com>
2025-05-09 20:32:21 +0200

    Correction des chemins d'inclusion dans PageController pour normaliser le nom du fichier globalTemplate.php

--- controllers/PageController.php
+++ controllers/PageController.php
@@ -4,22 +4,22 @@ class PageController{
 
     public static function index(){
         require_once(__DIR__ . '/../views/home.php');
-        require_once(__DIR__ . '/../templates/GlobalTemplate.php');
+        require_once(__DIR__ . '/../templates/globalTemplate.php');
     }
 
     public static function about(){
         require_once(__DIR__ . '/../views/about.php');
-        require_once(__DIR__ . '/../templates/GlobalTemplate.php');
+        require_once(__DIR__ . '/../templates/globalTemplate.php');
     }
 
     public static function blog(){
         require_once(__DIR__ . '/../views/blog.php');
-        require_once(__DIR__ . '/../templates/GlobalTemplate.php');
+        require_once(__DIR__ . '/../templates/globalTemplate.php');
     }
     
     public static function form(){
         require_once(__DIR__ . '/../views/form.php');
-        require_once(__DIR__ . '/../templates/GlobalTemplate.php');
+        require_once(__DIR__ . '/../templates/globalTemplate.php');
     }
 
 }
