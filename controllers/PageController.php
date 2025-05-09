82f1418a65fcd0cc0e5c0e703bffdb99f4e7f802
Lukas <lukas.boudinl@gmail.com>
2025-05-09 20:17:35 +0200

    Correction des chemins d'inclusion dans PageController pour supprimer les références relatives et assurer un accès direct aux fichiers

--- controllers/PageController.php
+++ controllers/PageController.php
@@ -3,23 +3,23 @@
 class PageController{
 
     public static function index(){
-        require_once('/../views/home.php');
-        require_once('/../templates/GlobalTemplate.php');
+        require_once('/views/home.php');
+        require_once('/templates/GlobalTemplate.php');
     }
 
     public static function about(){
-        require_once('/../views/about.php');
-        require_once('/../templates/GlobalTemplate.php');
+        require_once('/views/about.php');
+        require_once('/templates/GlobalTemplate.php');
     }
 
     public static function blog(){
-        require_once('/../views/blog.php');
-        require_once('/../templates/GlobalTemplate.php');
+        require_once('/views/blog.php');
+        require_once('/templates/GlobalTemplate.php');
     }
     
     public static function form(){
-        require_once('/../views/form.php');
-        require_once('/../templates/GlobalTemplate.php');
+        require_once('/views/form.php');
+        require_once('/templates/GlobalTemplate.php');
     }
 
 }
