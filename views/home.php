73aa38d78efc46409ff5ecf2356fd04b2b325b0e
Lukas <lukas.boudinl@gmail.com>
2025-05-14 16:02:49 +0200

    Correction de la numérotation des sections dans home.php pour une meilleure cohérence

--- views/home.php
+++ views/home.php
@@ -7,7 +7,7 @@ ob_start();
     <section class="intro">
         <div class="text">
             <h1>Entreprendre au Féminin</h1>
-            <h2>I. Introduction</h2>
+            <h2>Introduction</h2>
             <p>
                 L’entrepreneuriat est un sujet relativement connu de tous. Créer son activité et gagner en autonomie professionnelle, cela fait rêver, n’est-ce pas ? Malheureusement, la réalité est souvent plus complexe qu’on ne pourrait le penser. Dans cette étude, nous allons explorer le milieu entrepreneurial et mesurer à quel point il peut être difficile d’y évoluer en tant que femme. Avant d’approfondir les différents obstacles que peuvent rencontrer les femmes dans ce domaine, il convient de commencer par définir ce qu’est l’entrepreneuriat et de mettre en lumière l’importance de l’équité entre les sexes dans cet univers.<br>
             </p>
@@ -27,7 +27,7 @@ ob_start();
         <div class="card">
             <div class="card-text">
             <img src="https://images.pexels.com/photos/8923776/pexels-photo-8923776.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="Tendances Genre">
-            <h3>II. Les Tendances Entrepreneuriales par Genre</h3>
+            <h3>I. Les Tendances Entrepreneuriales par Genre</h3>
             <p></p>
             <a href="/blog#partie1">
                 <button>Lire plus</button>
@@ -37,7 +37,7 @@ ob_start();
         <div class="card">
             <div class="card-text">
             <img src="https://images.pexels.com/photos/6694482/pexels-photo-6694482.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="Défis financiers">
-            <h3>III. Obstacles et Défis Différenciés</h3>
+            <h3>II. Obstacles et Défis Différenciés</h3>
             <p></p>
             <a href="/blog#partie2">
                 <button>Lire plus</button>
@@ -47,7 +47,7 @@ ob_start();
         <div class="card">
             <img src="https://images.pexels.com/photos/7693153/pexels-photo-7693153.jpeg" alt="Facteurs de réussite">
             <div class="card-text">
-                <h3>IV. Facteurs de Réussite et Opportunités</h3>
+                <h3>III. Facteurs de Réussite et Opportunités</h3>
                 <p></p>
                 <a href="/blog#partie3">
                 <button>Lire plus</button>
